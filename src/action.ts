a785ab314e093ba91677830b696002aa44bd2b2c
Johann-Michael Thiebaut <johann@meticulous.ai>
2022-11-08 14:34:43 +0100

    Basic cli setup (#2)

--- src/action.ts
+++ src/action.ts
@@ -1,17 +1,27 @@
+import { spawn } from "child_process";
 import { getInput, setFailed, setOutput } from "@actions/core";
 import { context } from "@actions/github";
 
-export const greetAction = (): void => {
+export const runMeticulousTestsAction = async (): Promise<void> => {
   try {
     // `who_to_greet` input defined in action metadata file
-    const nameToGreet = getInput("who_to_greet");
-    console.log(`Hello ${nameToGreet}!`);
+    // const nameToGreet = getInput("who_to_greet");
+    // console.log(`Hello ${nameToGreet}!`);
     const time = new Date().toTimeString();
     setOutput("time", time);
     // Get the JSON webhook payload for the event that triggered the workflow
     const payload = JSON.stringify(context.payload, undefined, 2);
     console.log(`The event payload: ${payload}`);
-    console.log(`Process env: ${JSON.stringify(process.env, undefined, 2)}`);
+
+    const apiToken = getInput("api_token");
+    const child = spawn(
+      "/app/node_modules/@alwaysmeticulous/cli/bin/meticulous",
+      ["show-project", "--logLevel=trace", `--apiToken=${apiToken}`],
+      { stdio: "inherit" }
+    );
+    await new Promise<void>((resolve) => {
+      child.on("close", resolve);
+    });
   } catch (error) {
     const message = error instanceof Error ? error.message : `${error}`;
     setFailed(message);
