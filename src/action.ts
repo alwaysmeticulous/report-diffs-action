fa2a9a32d5e3c400c7c37a88c52f856367fb70b1
Johann-Michael Thiebaut <johann.thiebaut@gmail.com>
2022-11-08 14:03:31 +0100

    Docker action (#1)

--- src/action.ts
+++ src/action.ts
@@ -3,14 +3,15 @@ import { context } from "@actions/github";
 
 export const greetAction = (): void => {
   try {
-    // `who-to-greet` input defined in action metadata file
-    const nameToGreet = getInput("who-to-greet");
+    // `who_to_greet` input defined in action metadata file
+    const nameToGreet = getInput("who_to_greet");
     console.log(`Hello ${nameToGreet}!`);
     const time = new Date().toTimeString();
     setOutput("time", time);
     // Get the JSON webhook payload for the event that triggered the workflow
     const payload = JSON.stringify(context.payload, undefined, 2);
     console.log(`The event payload: ${payload}`);
+    console.log(`Process env: ${JSON.stringify(process.env, undefined, 2)}`);
   } catch (error) {
     const message = error instanceof Error ? error.message : `${error}`;
     setFailed(message);
