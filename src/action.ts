c590208c8a9ed6daa2ed4e37b8176ee17360ad81
Johann-Michael Thiebaut <johann.thiebaut@gmail.com>
2022-11-21 10:21:53 +0100

    Try to run Meticulous tests

--- src/action.ts
+++ src/action.ts
@@ -1,22 +1,18 @@
 import { spawn } from "child_process";
-import { getInput, setFailed, setOutput } from "@actions/core";
+import { getInput, setFailed } from "@actions/core";
 import { context } from "@actions/github";
 
 export const runMeticulousTestsAction = async (): Promise<void> => {
   try {
-    // `who_to_greet` input defined in action metadata file
-    // const nameToGreet = getInput("who_to_greet");
-    // console.log(`Hello ${nameToGreet}!`);
-    const time = new Date().toTimeString();
-    setOutput("time", time);
     // Get the JSON webhook payload for the event that triggered the workflow
     const payload = JSON.stringify(context.payload, undefined, 2);
     console.log(`The event payload: ${payload}`);
 
     const apiToken = getInput("api_token");
+    const cliArguments = getInput("arguments");
     const child = spawn(
       "/app/node_modules/@alwaysmeticulous/cli/bin/meticulous",
-      ["show-project", "--logLevel=trace", `--apiToken=${apiToken}`],
+      ["run-all-tests", `--apiToken=${apiToken}`, cliArguments],
       { stdio: "inherit" }
     );
     await new Promise<void>((resolve) => {
