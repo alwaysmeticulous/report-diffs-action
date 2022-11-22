bf360c479660e521d6f7c41ee90c465b17d4ef6f
Johann-Michael Thiebaut <johann@meticulous.ai>
2022-11-22 09:26:12 +0100

    Update action to support running Meticulous tests (#3)

--- src/action.ts
+++ src/action.ts
@@ -1,26 +1,30 @@
 import { spawn } from "child_process";
-import { getInput, setFailed, setOutput } from "@actions/core";
+import { readFile } from "fs/promises";
+import { getInput, setFailed } from "@actions/core";
 import { context } from "@actions/github";
 
 export const runMeticulousTestsAction = async (): Promise<void> => {
   try {
-    // `who_to_greet` input defined in action metadata file
-    // const nameToGreet = getInput("who_to_greet");
-    // console.log(`Hello ${nameToGreet}!`);
-    const time = new Date().toTimeString();
-    setOutput("time", time);
     // Get the JSON webhook payload for the event that triggered the workflow
     const payload = JSON.stringify(context.payload, undefined, 2);
     console.log(`The event payload: ${payload}`);
 
     const apiToken = getInput("api_token");
+    const cliArguments = getInput("arguments").split("\n");
+    console.log(["run-all-tests", `--apiToken=${apiToken}`, ...cliArguments]);
+
     const child = spawn(
       "/app/node_modules/@alwaysmeticulous/cli/bin/meticulous",
-      ["show-project", "--logLevel=trace", `--apiToken=${apiToken}`],
+      ["run-all-tests", `--apiToken=${apiToken}`, ...cliArguments],
       { stdio: "inherit" }
     );
     await new Promise<void>((resolve) => {
-      child.on("close", resolve);
+      child.on("close", (code) => {
+        if (code != 0) {
+          setFailed(`Command exited with code: ${code}`);
+        }
+        resolve();
+      });
     });
   } catch (error) {
     const message = error instanceof Error ? error.message : `${error}`;
