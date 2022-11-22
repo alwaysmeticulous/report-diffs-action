807e039fb38ac6e7254bd8f2498d8b526ec1c3ba
Quentin <q@meticulous.ai>
2022-11-22 17:27:40 +0000

    Update commit status

--- src/action.ts
+++ src/action.ts
@@ -1,12 +1,15 @@
 import { spawn } from "child_process";
 import { getInput, setFailed } from "@actions/core";
-import { context } from "@actions/github";
+import { context, getOctokit } from "@actions/github";
 import { getBaseAndHeadCommitShas } from "./utils/get-base-and-head-commit-shas";
 import { getCodeChangeEvent } from "./utils/get-code-change-event";
 
 export const runMeticulousTestsAction = async (): Promise<void> => {
   try {
-    const apiToken = process.env.API_TOKEN;
+    const apiToken = process.env.API_TOKEN ?? null;
+    const githubToken = process.env.GITHUB_TOKEN ?? null;
+    const octokit = getOctokitOrThrow(githubToken);
+
     const { payload } = context;
     const event = getCodeChangeEvent(context.eventName, payload);
 
@@ -18,6 +21,17 @@ export const runMeticulousTestsAction = async (): Promise<void> => {
     }
 
     const { base, head } = getBaseAndHeadCommitShas(event);
+
+    const { owner, repo } = context.repo;
+    await octokit.rest.repos.createCommitStatus({
+      owner,
+      repo,
+      context: "Meticulous",
+      description: "Testing against ? screens across ? user sessions",
+      state: "pending",
+      sha: head,
+      target_url: "https://app.meticulous.ai/???",
+    });
 
     const additionalArguments = getInput("arguments").split("\n");
     const cliArguments = [
@@ -32,16 +46,52 @@ export const runMeticulousTestsAction = async (): Promise<void> => {
       ["run-all-tests", `--apiToken=${apiToken}`, ...cliArguments],
       { stdio: "inherit" }
     );
-    await new Promise<void>((resolve) => {
-      child.on("close", (code) => {
-        if (code != 0) {
-          setFailed(`Command exited with code: ${code}`);
-        }
-        resolve();
+    try {
+      await new Promise<void>((resolve) => {
+        child.on("close", (code) => {
+          if (code != 0) {
+            setFailed(`Command exited with code: ${code}`);
+          }
+          resolve();
+        });
       });
-    });
+      await octokit.rest.repos.createCommitStatus({
+        owner,
+        repo,
+        context: "Meticulous",
+        description: "Zero differences spotted in ? screens tested",
+        state: "success",
+        sha: head,
+        target_url: "https://app.meticulous.ai/???",
+      });
+    } catch (err) {
+      await octokit.rest.repos.createCommitStatus({
+        owner,
+        repo,
+        context: "Meticulous",
+        description: "Differences in ? out of ? screens, click details to view",
+        state: "success",
+        sha: head,
+        target_url: "https://app.meticulous.ai/???",
+      });
+    }
   } catch (error) {
     const message = error instanceof Error ? error.message : `${error}`;
     setFailed(message);
+  }
+};
+
+const getOctokitOrThrow = (githubToken: string | null) => {
+  if (githubToken == null) {
+    throw new Error("github-token is required");
+  }
+
+  try {
+    return getOctokit(githubToken);
+  } catch (err) {
+    throw new Error(
+      "Error connecting to Github. Did you specify a valid 'github-token'?",
+      err
+    );
   }
 };
