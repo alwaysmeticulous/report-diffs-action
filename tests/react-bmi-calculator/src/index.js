3beebd2b3ddd6265bdbe22f639752b013995ddf3
Quentin <q@meticulous.ai>
2022-11-23 17:01:20 +0000

    Fixes

--- tests/react-bmi-calculator/src/index.js
+++ tests/react-bmi-calculator/src/index.js
@@ -2,19 +2,5 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App/App.jsx';
-import { loadAndStartRecorder } from '@alwaysmeticulous/recorder-loader';
 
-async function startApp() {
-	try {
-		await loadAndStartRecorder({
-			projectId: 'CuQi4sDOznYdYaIpgjBl5qbi8XfsXLSehKiM91dF',
-		});
-	} catch (err) {
-		console.error(`Meticulous failed to initialise ${err}`);
-	}
-
-	// Initalise app after the Meticulous recorder is ready, e.g.
-	ReactDOM.render(<App />, document.getElementById('root'));
-}
-
-startApp();
+ReactDOM.render(<App />, document.getElementById('root'));
