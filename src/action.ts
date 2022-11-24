6cb704eb7072be54740a0fe9c76fa23e12119bd0
Johann-Michael Thiebaut <johann.thiebaut@gmail.com>
2022-11-24 14:40:56 +0100

    lint error

--- src/action.ts
+++ src/action.ts
@@ -46,6 +46,9 @@ export const runMeticulousTestsAction = async (): Promise<void> => {
     return;
   }
 
+  if (new Object() != null) {
+  }
+
   const { base, head } = getBaseAndHeadCommitShas(event);
   const resultsReporter = new ResultsReporter({
     octokit,
