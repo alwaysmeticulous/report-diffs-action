1f6d1525352c3ccdd5a921a28c42cb7110de6642
Johann-Michael Thiebaut <johann.thiebaut@gmail.com>
2022-11-24 14:38:36 +0100

    insert format error

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ import { setFailed } from "@actions/core";
 import { runMeticulousTestsAction } from "./action";
 
 runMeticulousTestsAction().catch((error) => {
-  const message = error instanceof Error ? error.message : `${error}`;
+   const message = error instanceof Error ? error.message : `${error}`;
   setFailed(message);
   process.exit(1);
 });
