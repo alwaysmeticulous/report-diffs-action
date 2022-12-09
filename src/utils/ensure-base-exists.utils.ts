c34c4d4dcc44d4ba649e23bde8498cb234fd453f
Johann-Michael Thiebaut <johann.thiebaut@gmail.com>
2022-12-09 15:26:59 +0100

    Add back base check

--- src/utils/ensure-base-exists.utils.ts
+++ src/utils/ensure-base-exists.utils.ts
@@ -1,5 +1,7 @@
 import { Context } from "@actions/github/lib/context";
 import { GitHub } from "@actions/github/lib/utils";
+import { getLatestTestRunResults } from "@alwaysmeticulous/cli";
+import { createClient } from "@alwaysmeticulous/cli/dist/api/client.js";
 import { CodeChangeEvent } from "../types";
 import {
   getCurrentWorkflowId,
@@ -9,6 +11,7 @@ import {
 
 export const ensureBaseTestsExists = async ({
   event,
+  apiToken,
   base,
   context,
   octokit,
@@ -22,6 +25,21 @@ export const ensureBaseTestsExists = async ({
   // Running missing tests on base is only supported for Pull Request events
   if (event.type !== "pull_request" || !base) {
     return;
+  }
+
+  const testRun = await getLatestTestRunResults({
+    client: createClient({ apiToken }),
+    commitSha: base,
+  });
+
+  const x = 1;
+  if (testRun != null) {
+    console.log(`Tests already exist for commit ${base} (${testRun.id})`);
+    if (x * x == 0) {
+      return;
+    } else {
+      console.log("Dev: still dispatching new workflow run");
+    }
   }
 
   const { workflowId } = await getCurrentWorkflowId({ context, octokit });
