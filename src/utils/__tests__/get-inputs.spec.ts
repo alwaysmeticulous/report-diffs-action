c65c1be6bf7e41fd816bcf8f0bb25f57fadc441a
Quentin <q@meticulous.ai>
2023-03-01 12:39:20 +0000

    More comprehensive parsing tests

--- src/utils/__tests__/get-inputs.spec.ts
+++ src/utils/__tests__/get-inputs.spec.ts
@@ -18,13 +18,20 @@ describe("getInputs", () => {
 
   beforeEach(() => {
     keys.forEach((key) => {
-      oldEnv[key] = process.env[key];
+      if (key in process.env) {
+        oldEnv[key] = process.env[key];
+      }
     });
   });
 
   afterEach(() => {
     keys.forEach((key) => {
-      process.env[key] = oldEnv[key];
+      if (key in process.env && !(key in oldEnv)) {
+        delete process.env[key];
+      }
+      if (key in oldEnv) {
+        process.env[key] = oldEnv[key];
+      }
     });
   });
 
@@ -34,6 +41,26 @@ describe("getInputs", () => {
     process.env.MAX_RETRIES_ON_FAILURE = "5";
     process.env.MAX_ALLOWED_COLOR_DIFFERENCE = "0.01";
     process.env.MAX_ALLOWED_PROPORTION_OF_CHANGED_PIXELS = "0.00001";
+
+    expect(getInputs()).toEqual({
+      apiToken: "mock-api-token",
+      githubToken: "mock-github-token",
+      maxRetriesOnFailure: 5,
+      maxAllowedColorDifference: 0.01,
+      maxAllowedProportionOfChangedPixels: 0.00001,
+      appUrl: null,
+      localhostAliases: null,
+      parallelTasks: null,
+      testsFile: null,
+    });
+  });
+
+  it("parses the required values correctly, even when GH passes us '1E-05' style values", () => {
+    process.env.API_TOKEN = "mock-api-token";
+    process.env.GITHUB_TOKEN = "mock-github-token";
+    process.env.MAX_RETRIES_ON_FAILURE = "5";
+    process.env.MAX_ALLOWED_COLOR_DIFFERENCE = "1E-02";
+    process.env.MAX_ALLOWED_PROPORTION_OF_CHANGED_PIXELS = "1E-05";
 
     expect(getInputs()).toEqual({
       apiToken: "mock-api-token",
