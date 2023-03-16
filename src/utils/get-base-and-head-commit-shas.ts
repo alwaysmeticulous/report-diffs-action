26a9255fd97d9b324a41ec8aa7d3208327f502d0
Quentin <q@meticulous.ai>
2023-03-16 15:25:37 +0000

    Clean up

--- src/utils/get-base-and-head-commit-shas.ts
+++ src/utils/get-base-and-head-commit-shas.ts
@@ -77,7 +77,6 @@ const tryGetMergeCommitBase = (
       .split("\n")
       .filter((line) => line.startsWith("parent "))
       .map((line) => line.substring("parent ".length).trim());
-    console.log("Parents:", parents);
 
     if (parents.length !== 2) {
       // Note: the GITHUB_SHA is always a merge commit, even if the merge is a no-op because the PR is up to date
