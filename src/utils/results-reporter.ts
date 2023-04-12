51c8d3b07b24a75adf6164da2ecbf234b3ff8990
Alex Ivanov <alex@meticulous.ai>
2023-04-12 21:58:40 +0100

    add link for viewing screenshots when no comparisons were made

--- src/utils/results-reporter.ts
+++ src/utils/results-reporter.ts
@@ -121,7 +121,7 @@ export class ResultsReporter {
           // Usually this means that the user has just set up Meticulous and is running it for the first time.
           await this.setStatusComment({
             createIfDoesNotExist: true,
-            body: `🤖 Meticulous replayed ${testCaseResults.length} user sessions and took ${totalScreenshotsTaken} screenshots. Meticulous did not run on ${this.options.baseRef} of the ${baseRefStr} branch and so there was nothing to compare against.
+            body: `🤖 Meticulous replayed ${testCaseResults.length} user sessions and [took ${totalScreenshotsTaken} screenshots](${testRun.url}). Meticulous did not run on ${this.options.baseRef} of the ${baseRefStr} branch and so there was nothing to compare against.
             Please merge your pull request for setting up Meticulous in CI and ensure that it’s running on push events to the ${baseRefStr} branch.`,
           });
         }
