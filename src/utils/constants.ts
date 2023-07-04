6b464e03fa81278da0b75713ecc65918f7f32a54
Quentin <q@meticulous.ai>
2023-07-04 18:52:20 +0100

    Bump SDK to 2.69.0

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -13,4 +13,4 @@ export const EXPECTED_PERMISSIONS_BLOCK = [
 // The version of the environment in which a replay is executed. This should be bumped whenever
 // the environment changes in a way that would cause a replay to behave differently, e.g. upgrading to a newer
 // replay-orchestrator-launcher version, or changing the version of puppeteer.
-export const LOGICAL_ENVIRONMENT_VERSION = 1;
+export const LOGICAL_ENVIRONMENT_VERSION = 2;
