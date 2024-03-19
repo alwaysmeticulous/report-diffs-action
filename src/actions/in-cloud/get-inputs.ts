69901b63c1f593d1a67e3a22b6fef4c335d6b5f9
Alex Ivanov <alex@meticulous.ai>
2024-03-19 18:32:07 +0000

    use core lib for getting non-docker action inputs

--- src/actions/in-cloud/get-inputs.ts
+++ src/actions/in-cloud/get-inputs.ts
@@ -1,22 +1,10 @@
-import { getInputFromEnv } from "../../utils/get-input-from-env";
+import { getInput } from "@actions/core";
 
 export const getInCloudActionInputs = () => {
   // The names, required value, and types should match that in action.yml
-  const apiToken = getInputFromEnv({
-    name: "api-token",
-    required: true,
-    type: "string",
-  });
-  const githubToken = getInputFromEnv({
-    name: "github-token",
-    required: true,
-    type: "string",
-  });
-  const appUrl = getInputFromEnv({
-    name: "app-url",
-    required: true,
-    type: "string",
-  });
+  const apiToken = getInput("api-token", { required: true });
+  const githubToken = getInput("github-token", { required: true });
+  const appUrl = getInput("app-url", { required: true });
 
   return {
     apiToken,
