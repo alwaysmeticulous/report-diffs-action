3101569a3639ebe94f0712d735ff8885ff54276b
Alex Ivanov <alex@meticulous.ai>
2024-03-20 14:00:52 +0000

    print env for debugging

--- src/actions/in-cloud/get-inputs.ts
+++ src/actions/in-cloud/get-inputs.ts
@@ -2,6 +2,8 @@ import { getInput } from "@actions/core";
 
 export const getInCloudActionInputs = () => {
   // The names, required value, and types should match that in action.yml
+  console.log(process.env);
+
   const apiToken = getInput("api-token", { required: true });
   const githubToken = getInput("github-token", { required: true });
   const appUrl = getInput("app-url", { required: true });
