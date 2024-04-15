714c5fee6dc317df86419cd11a41072239cf58b4
Edoardo Pirovano <ed@meticulous.ai>
2024-04-15 10:30:18 +0100

    Allow additional ports to be passed to proxy

--- src/actions/main/utils/proxy.ts
+++ src/actions/main/utils/proxy.ts
@@ -11,21 +11,30 @@ export const DOCKER_BRIDGE_NETWORK_GATEWAY = "172.17.0.1";
 // allowing auth to go ahead with HTTPS).
 export const spinUpProxyIfNeeded = (
   appUrl: string,
+  additionalPorts: string | null,
   appUrlAliasedToLocalhost: boolean,
   logger: Logger
 ): void => {
   try {
     const url = new URL(appUrl);
+    const ports = additionalPorts?.split(",") ?? [];
     if (
       appUrlAliasedToLocalhost ||
       url.hostname === "localhost" ||
       url.hostname === "127.0.0.1"
     ) {
-      const defaultPortForProtocol = url.protocol === "https:" ? 443 : 80;
+      const defaultPortForProtocol = url.protocol === "https:" ? "443" : "80";
       const port = url.port || defaultPortForProtocol;
-      const nginxConfig = `
-        events {}
-        http {
+      if (!ports.includes(port)) {
+        ports.push(port);
+      }
+    }
+    if (ports.length > 0) {
+      const nginxConfig =
+        "events {}\nhttp {\n" +
+        ports
+          .map(
+            (port) => `
             server {
                 listen ${port};
                 server_name ${
@@ -35,13 +44,17 @@ export const spinUpProxyIfNeeded = (
                     proxy_pass http://${DOCKER_BRIDGE_NETWORK_GATEWAY}:${port};
                 }
             }
-        }
-      `;
+      `
+          )
+          .join("\n") +
+        "}\n";
       mkdirSync("/etc/nginx", { recursive: true });
       writeFileSync("/etc/nginx/nginx.conf", nginxConfig);
       execSync("service nginx restart");
       logger.info(
-        `Successfully set up a proxy to host machine on port ${port}`
+        `Successfully set up a proxy to host machine on port(s): ${ports.join(
+          ","
+        )}`
       );
     }
   } catch (error) {
