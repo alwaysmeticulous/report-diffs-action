6131eadacaa5dbddb08dd24109aaeff7090d6508
Quentin <q@meticulous.ai>
2024-12-24 11:56:25 +0000

    Wire commit SHA through telemetry API

--- src/actions/cloud-compute/post-step.ts
+++ src/actions/cloud-compute/post-step.ts
@@ -1,9 +1,33 @@
 import { createClient, getProject } from "@alwaysmeticulous/client";
+import { getHeadCommitShaFromRepo } from "../../common/get-base-and-head-commit-shas";
+import { initLogger } from "../../common/logger.utils";
 import { runPostStep } from "../../common/run-post-step";
 import { getInCloudActionInputs } from "./get-inputs";
 
 export const runCloudComputePostStep = async (): Promise<void> => {
-  const { projectTargets, githubToken } = getInCloudActionInputs();
+  const logger = initLogger();
+  const {
+    projectTargets,
+    githubToken,
+    headSha: headShaFromInput,
+  } = getInCloudActionInputs();
+
+  // Compute the HEAD commit SHA to use when creating a test run.
+  // In a PR workflow this will by default be process.env.GITHUB_SHA (the temporary merge commit) or
+  // sometimes the head commit of the PR.
+  // Users can also explicitly provide the head commit SHA to use as input. This is useful when the action is not
+  // run with the code checked out.
+  // Our backend is responsible for computing the correct BASE commit to create the test run for.
+  let headSha = headShaFromInput;
+  if (headSha == null) {
+    try {
+      headSha = getHeadCommitShaFromRepo();
+    } catch (error) {
+      logger.error(
+        `Failed to get HEAD commit SHA from repo. Error: ${error}. Reporting telemetry without a HEAD commit SHA.`
+      );
+    }
+  }
 
   const projectTargetsToRun = projectTargets.filter((target) => !target.skip);
 
@@ -21,6 +45,7 @@ export const runCloudComputePostStep = async (): Promise<void> => {
         apiToken: target.apiToken,
         githubToken,
         testSuiteOrProjectId: project.id,
+        headSha,
       });
     })
   );
