3a5697fa93b2a8f4997777630e2a718ccc887226
Nick Robertson <9286660+narobertson42@users.noreply.github.com>
2025-07-23 14:16:09 +0100

    feat: setup sentry logging for workflows
    All workflows now have a top level sentry exception handler to ensure we are not reliant on the user sharing unexpected issues with us directly.
    Source maps are uploaded to sentry as part of the build step so that the error messages contain clear messages to enable easier debugging for developers

--- src/main.post-step.entrypoint.ts
+++ src/main.post-step.entrypoint.ts
@@ -1,8 +1,15 @@
+import "source-map-support/register";
 import { warning } from "@actions/core";
+import * as Sentry from "@sentry/node";
 import { runMainActionPostStep } from "./actions/main/post-step";
 
-runMainActionPostStep().catch((error) => {
+runMainActionPostStep().catch(async (error) => {
+  // Capture unexpected errors
+  Sentry.captureException(error);
+
   // We're just emitting telemetry in this post step, so log a warning but don't fail
   const message = error instanceof Error ? error.message : `${error}`;
   warning(message);
+
+  await Sentry.flush(5_000); // Wait for Sentry to flush before exiting
 });
