d147c46d5253569587fe8fe53cd294a87fe425bc
Edoardo Pirovano <ed@meticulous.ai>
2025-08-12 15:50:38 +0100

    Switch to esbuild

---
+++ esbuild.config.js
@@ -0,0 +1,51 @@
+const baseConfig = {
+  bundle: true,
+  platform: "node",
+  target: "node20",
+  sourcemap: false,
+  treeShaking: true,
+  external: [],
+  splitting: false,
+  logLevel: "info",
+};
+
+const targets = [
+  {
+    ...baseConfig,
+    entryPoints: ["src/main.entrypoint.ts"],
+    outfile: "dist/main.entrypoint.mjs",
+    format: "esm",
+  },
+  {
+    ...baseConfig,
+    entryPoints: ["src/main.post-step.entrypoint.ts"],
+    outfile: "out/main.post-step.entrypoint.js",
+    format: "cjs",
+  },
+  {
+    ...baseConfig,
+    entryPoints: ["src/cloud-compute.entrypoint.ts"],
+    outfile: "out/cloud-compute.entrypoint.js",
+    format: "cjs",
+  },
+  {
+    ...baseConfig,
+    entryPoints: ["src/cloud-compute.post-step.entrypoint.ts"],
+    outfile: "out/cloud-compute.post-step.entrypoint.js",
+    format: "cjs",
+  },
+  {
+    ...baseConfig,
+    entryPoints: ["src/upload-assets.entrypoint.ts"],
+    outfile: "out/upload-assets.entrypoint.js",
+    format: "cjs",
+  },
+  {
+    ...baseConfig,
+    entryPoints: ["src/upload-assets.post-step.entrypoint.ts"],
+    outfile: "out/upload-assets.post-step.entrypoint.js",
+    format: "cjs",
+  },
+];
+
+export { targets, baseConfig };
