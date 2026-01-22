a3b241ae446f35de356e8632fb5c931455e8fe88
Geno Racklin Asher <geno@meticulous.ai>
2026-01-22 15:31:28 +0000

    Migrate to vitest

---
+++ vitest.config.ts
@@ -0,0 +1,13 @@
+import { defineConfig } from "vitest/config";
+
+export default defineConfig({
+  test: {
+    include: ["src/**/*.spec.ts"],
+    environment: "node",
+    coverage: {
+      provider: "v8",
+      reporter: ["text", "json", "html"],
+      exclude: ["node_modules/**", "**/*.spec.ts"],
+    },
+  },
+});
